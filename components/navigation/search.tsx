320e70afbea3aa51e3c4c862f0ad39e47821de0e
onlyeugene <eugeneokogun@gmail.com>
2025-03-14 23:21:14 +0100

    feat: fixed the services and cart section

--- components/navigation/search.tsx
+++ components/navigation/search.tsx
@@ -5,7 +5,7 @@ import { SearchIcon } from "lucide-react";
 const Search = () => {
   return (
     <div className="relative w-[40rem] md:block hidden">
-      <Input placeholder="Search" type="search"  className="px-10 focus:border-none focus-visible:ring-[1px] rounded-full placeholder:text-black"/>
+      <Input placeholder="Search" type="search"  className="px-10 focus:border-none focus-visible:ring-[1px] border-black rounded-full placeholder:text-black"/>
       <SearchIcon className="absolute top-2 left-3"  size={14}/>
     </div>
   );
